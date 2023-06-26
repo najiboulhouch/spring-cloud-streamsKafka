2c2c9e810e17c44ec7daad77d96cff29c6de66da
Najib OULHOUCH <developpersgroups@gmail.com>
2023-06-26 14:14:06 +0200

    Add my code to Git

---
+++ src/main/java/net/najiboulhouch/demospringcloudstreamskafka/DemoSpringcloudStreamskafkaApplication.java
@@ -0,0 +1,13 @@
+package net.najiboulhouch.demospringcloudstreamskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringcloudStreamskafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringcloudStreamskafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/najiboulhouch/demospringcloudstreamskafka/DemoSpringcloudStreamskafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.najiboulhouch.demospringcloudstreamskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringcloudStreamskafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
